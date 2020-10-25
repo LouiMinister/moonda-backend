6ae618952d6b40587c9e04a6e6016d526440a8e5
LouiMinister <LouiMinister@gmail.com>
2020-10-25 17:22:50 +0900

    Update pm2 echosystem config file
    클러스터링 환경을 위해 interpreter 옵션을 server-register.js 로 대체함

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports =  {
   apps : [
     {
       name: "moonda-restapi",
-      script: "./app.js",
+      script: "./server-register.js",
       watch: true,
-      interpreter: "node_modules/@babel/node/bin/babel-node.js",
       instance: 0,
       exec_mode: 'cluster',
       listen_timeout: 50000
